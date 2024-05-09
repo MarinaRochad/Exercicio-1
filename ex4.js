9c4e329ad034b2a1526c670858626c9bd37d53b2
MARINA ROCHA DA SILVA <47079439850@info.local>
2024-05-09 10:53:11 -0300

    continuando

---
+++ ex4.js
@@ -0,0 +1,21 @@
+
+
+
+//Exercício 4: Notas escolares
+//Faça um programa que receba 4 notas de um aluno, 
+//calcule e imprima a média aritmética das notas
+//e a mensagem de APROVADO para média superior ou igual a 7,0 RECUPERAÇÃO 
+//para notas entre 5.0 e 7,0 ou a mensagem de REPROVADO para média inferior a 5,0.
+
+let nota1 = parseInt(prompt("Digite uma nota: "));
+let nota2 = parseInt (prompt ("Digite outra nota: "));
+let nota3 = parseInt(prompt ("Digite outra nota: "));
+let nota4 = parseInt(prompt("Digite outra nota: "));
+
+let media= (nota1+nota2+nota3+nota4) /2
+
+console.log (media)
+
+
+
+
