5f3cd76cd38e40105a7578b2181c7e3abfca90fd
MARINA ROCHA DA SILVA <47079439850@info.local>
2024-05-09 08:43:09 -0300

    exercicio 1

---
+++ exemplo.js
@@ -0,0 +1,41 @@
+//apresentar alguma mensagem na tela 
+
+///alert("Hello Word!!")
+
+//console.log("Hello Word!!")
+
+
+//Escreva alguma coisa que solicite o nome do usuário e lhe de boas vindas, exibindo uma mensagem pelo Console.
+
+//prompt() -> recebe uma entrada digitada
+let nome = prompt("Olá, digite o seu nome: ");
+
+//Concatenação: Juntar texto 
+//console.log("Seja bem vindo" + nome + "!!!");
+
+
+//adicionar pontos para o usu[ario]
+let pontos = 100;
+
+//parseInt ()-> Transforma o que esta entre () em numero inteiro
+//let bonus= parseInt(prompt("Quantos pontos você deseja somar?"));
+
+
+
+//Adição -> num1+ num2
+//Subtração -> num1 - num2
+//Multiplicação -> num1 * num2
+//Divisão -> num1 / num2
+
+
+//conta:
+//let totalPontos = pontos + bonus;
+
+//para mostrar o resultado da conta:
+//console.log(totalPontos);
+
+
+//parseFloat --> Transforma o que esta ente () em numero quebrado.
+let notAluno = parseFloat(prompt("Digite a nota do aluno: "));
+console.log(notAluno);
+
