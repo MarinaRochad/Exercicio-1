d4d4366e916495df61e7202d1a01ebd1c64b9a33
MARINA ROCHA DA SILVA <47079439850@info.local>
2024-05-09 10:20:41 -0300

    Exercicio04

---
+++ ex3.js
@@ -0,0 +1,13 @@
+//Exercício 3: Ímpar ou par
+//Faça um programa que receba um número do usuário e informe se este número é par ou
+//ímpar.//
+
+alert ("Seja bem Vindo!")
+
+let número = prompt ("Coloque um número e descruba se ele é ímpar ou par!")
+
+if (número %2 == 0) {
+    alert ("Par")
+} else {
+    alert ("Ímpar")
+}
