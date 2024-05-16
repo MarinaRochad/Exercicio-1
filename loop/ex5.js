6217c02589df9272883f86fb47797496297e0314
MARINA ROCHA DA SILVA <47079439850@info.local>
2024-05-16 09:45:32 -0300

    exercicio5

--- loop/ex5.js
+++ loop/ex5.js
@@ -5,6 +5,22 @@
 // 5. Imprimir os primeiros 10 números da sequência de Fibonacci.
 
 
-for( let indice = 0; indice < 1,1,2,3,5,8,13,21,34,55 ); 
+let fib = 1;
+let fib2 = 1;
 
+console.log("Os primeiros 10 numeros da sequencia de Fibonacci sao: ")
+console.log(fib1);
+console.log(fib2);
+
+//para calcular e imprimir os proximos numeros da sequencia.
+
+for(let i = 2; i <10; i++){
+    let nextFib = fib1 + fib2;
+    console.log(nextFib);
+
+    //Atualizar os valores de fib1 e fib2 para os proximos calculos
+    fib1 = fib2;
+    fib2 = nextFib;
+
+}
 
