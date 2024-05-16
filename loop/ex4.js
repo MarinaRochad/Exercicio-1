3b49a28ea3809686f1c119a1f67c7374edec58e9
MARINA ROCHA DA SILVA <47079439850@info.local>
2024-05-16 09:39:13 -0300

    exercicio4

--- loop/ex4.js
+++ loop/ex4.js
@@ -5,6 +5,17 @@
 // 4. Faça um programa que imprima na tela a tabuada de um número
 // qualquer escolhido pelo usuário até o 10.
 
-for (let cont = 0; cont >= 10; cont ++){
-    console.log (contador)
+let numero = parseInt(prompt("Digite um numero e veja sua tabuada ate o 10"));
+
+if(!isNaN(numero)) {
+    for(let cont = 1; cont <=; cont++) {
+        console.log(numero + "X" + cont + "=" + (numero * cont));
+    }
+
 }
+else{
+    console.log("Numero invalido. Por favor, digite um numero valido")
+}
+
+console.log(isNan("Hello Word")); //retorna true
+console.log(isNan(123));//retorna false
