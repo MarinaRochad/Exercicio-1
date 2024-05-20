b57954c858eb07d0ebb5f5d656971a2be8d2c854
MARINA ROCHA DA SILVA <47079439850@info.local>
2024-05-20 10:49:47 -0300

    dom

---
+++ dom/exercicios.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+//1-Alterar o texto de um paragrafo ao clicar em um botao 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//2-Adicionar e remover itens de uma lista 
+
+//3-Fechar a pagina, voltar, mostrar sua localizacao
--- dom/index.js
+++ dom/index.js
@@ -4,3 +4,47 @@ let titulo = document.getElementById("titulo")
 
 titulo.style.fontFamily = "Segoe UI"
 titulo.style.color = "red"
+
+//selecione o elemento com ID "titulo" do documento (document)
+let titulo = window.document.getElementById("titulo")
+
+//define a cor do texto do elemento "titulo"
+titulo.style.color = "#002596"
+
+//define atipografia do elemento 
+titulo.style.fontFamily = "Montserrat"
+
+//inicialia uma variavel 
+let clique= 0;
+
+//fun;'ao para contagem de cliques 
+const somarCliques = () => {
+
+    //incrementou 1 valor da variavel
+    Clique++
+
+    //Seleciona o botao por querSelector e atualiza a quantidade de cliques no HTML
+    document.querySelector("#botao").innerHTML = clique
+}
+
+const adicionarItem = () => {
+    //Criar um novo elemento li(item da lista)
+    let novoItem = document.createElement("li")
+    //Define o texto do no item
+    novoItem.textContent = "Novo Item"
+    //Identifica o elemento que vai ser adicionado
+    let lista = document.getElementById("minhaLista")
+    //Adiciona um novo item a lista
+    lista.appendChild(novoItem);
+}
+
+const voltarPagina = () =>{
+    window.history.back()
+}
+
+const fecharPagina = () => {
+    window.close()
+}
+
+
+
