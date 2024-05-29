1eb94c341136129cd01bc1498ca2c5f6b7db7846
MARINA ROCHA DA SILVA <47079439850@info.local>
2024-05-29 08:01:29 -0300

    exercicio2

--- loop/ex2.js
+++ loop/ex2.js
@@ -3,7 +3,14 @@
 
 // 2. Imprimir números pares de 1 a 10.
 
+for(let num = 1; num <=10; num++){
+    if(num%2 == 0){
+        console.log(num);
+    }
+}
 
-for(let cont = 2; cont >= 10; cont ++){ 
-console.log(cont);
+console.log("------------------------")
+
+for(let cont = 2; cont <= 10; cont+=2){
+    console.log(cont)
 }
